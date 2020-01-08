e383d97dccd770c4bd58ccde5211b5fc5f1658d8
mcheifetz <33294677+mcheifetz@users.noreply.github.com>
2020-01-08 15:29:59 -0500

    Initial Comments

---
+++ Main.py
@@ -0,0 +1,4 @@
+# Terminal Display class: One class will recieve card data and will render the letters and the ASCII hangman
+    # Instance of the Hangman Class
+
+# Actual hangman class: Take user input (letter) and check if it's in a word
