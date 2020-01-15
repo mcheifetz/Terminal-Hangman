8b96c4b31411e87bd3c0d03882efb3cff68556fe
mcheifetz <33294677+mcheifetz@users.noreply.github.com>
2020-01-15 15:47:53 -0500

    Added

--- Main.py
+++ Main.py
@@ -1,4 +1,41 @@
-# Terminal Display class: One class will recieve card data and will render the letters and the ASCII hangman
-    # Instance of the Hangman Class
+#List of possible letters
+letters = {'abcdefghijklmnopqrstuvwxyz'}
 
-# Actual hangman class: Take user input (letter) and check if it's in a word
+def travelToPoint(x, y):
+	print(("\u001b[%s;%sH").format(y, x))
+
+def startGame():
+	secretPhrase = str(input("Enter the secret phrase: "))
+	guessedPhrase = ""
+
+	startingX = 4
+	while True:
+		# Clear screen
+		print("\u001b[2J")
+		# Travel to top left corner of screen
+		travelToPoint(startingX, 2)
+		# Turn on underscoring
+		for index in range(len(secretPhrase)):
+			# Don't put an underscore if there is a space
+			if secretPhrase[index] == " ":
+				print("\u001b[m")
+			else:
+				# Print an underscore to represent the hidden letter
+				print("\u001b[4m")
+				# Check to see if the letter has been guessed
+				if index < len(guessedPhrase) and guessedPhrase[index] != " ":
+					# The letter has been guessed, write it
+					print(guessedPhrase[index], end = "")
+		# Remove underlining because done
+		print("\u001b[m")
+		# Ask for next letter
+		letter = str(input("Enter letter: "))
+		for index in range(len(secretPhrase)):
+			if secretPhrase[index] == letter:
+				# The letter has been guessed
+				tempText = list(guessedPhrase)
+				tempText[index] = letter
+				guessedPhrase = "".join(tempText)
+
+# Start the game
+startGame()
