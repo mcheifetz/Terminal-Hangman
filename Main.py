93946a622808fbe44725017791bbaa01c9ea1664
TheGreatRambler <aehart684351@gmail.com>
2020-01-22 15:32:59 -0500

    Did a thing
    Co-authored-by: vivekmatta <vmatta2002@gmail.com>

--- Main.py
+++ Main.py
@@ -6,7 +6,7 @@ def travelToPoint(x, y):
 
 def startGame():
 	secretPhrase = str(input("Enter the secret phrase: "))
-	guessedPhrase = ""
+	guessedPhrase = " " * len(secretPhrase)
 
 	startingX = 4
 	while True:
@@ -18,14 +18,18 @@ def startGame():
 		for index in range(len(secretPhrase)):
 			# Don't put an underscore if there is a space
 			if secretPhrase[index] == " ":
-				print("\u001b[m")
+				print("\u001b[m ",end='')
 			else:
 				# Print an underscore to represent the hidden letter
-				print("\u001b[4m")
+				print("\u001b[4m",end='')
 				# Check to see if the letter has been guessed
 				if index < len(guessedPhrase) and guessedPhrase[index] != " ":
 					# The letter has been guessed, write it
 					print(guessedPhrase[index], end = "")
+				else:
+					print(" ", end='')
+			
+		print("\u001b[m",end='')
 		# Remove underlining because done
 		print("\u001b[m")
 		# Ask for next letter
@@ -34,7 +38,7 @@ def startGame():
 			if secretPhrase[index] == letter:
 				# The letter has been guessed
 				tempText = list(guessedPhrase)
-				tempText[index] = letter
+				tempText.insert(index, letter);
 				guessedPhrase = "".join(tempText)
 
 # Start the game
