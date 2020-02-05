b34f7e228797fb90a306f607e383d3e28a707711
mcheifetz <33294677+mcheifetz@users.noreply.github.com>
2020-02-05 15:50:25 -0500

    Finally works

--- Main.py
+++ Main.py
@@ -1,41 +1,79 @@
-#List of possible letters
-letters = {'abcdefghijklmnopqrstuvwxyz'}
-
-def travelToPoint(x, y):
-	print(("\u001b[%s;%sH").format(y, x))
+# import only system from os 
+from os import system, name 
+  
+# import sleep to show output for some time period 
+from time import sleep 
+  
+# define our clear function 
+def clear(): 
+  
+    # for windows 
+    if name == 'nt': 
+        _ = system('cls') 
+  
+    # for mac and linux(here, os.name is 'posix') 
+    else: 
+        _ = system('clear') 
+  
+  
 
 def startGame():
-	secretPhrase = str(input("Enter the secret phrase: "))
-	guessedPhrase = ""
-
-	startingX = 4
+	clear()
+	secretPhrase = list(str(input("Enter the secret phrase: ")))
+	clear()
+	guessedPhrase = [" "] * len(secretPhrase)
+	lives = 10
+	
 	while True:
-		# Clear screen
-		print("\u001b[2J")
-		# Travel to top left corner of screen
-		travelToPoint(startingX, 2)
-		# Turn on underscoring
+		# Writing the blank word
+		haveWon = True
 		for index in range(len(secretPhrase)):
-			# Don't put an underscore if there is a space
+			# Don't put an underscore if there's a space
 			if secretPhrase[index] == " ":
-				print("\u001b[m")
+ 				print(" ", end = " ")
 			else:
-				# Print an underscore to represent the hidden letter
-				print("\u001b[4m")
-				# Check to see if the letter has been guessed
-				if index < len(guessedPhrase) and guessedPhrase[index] != " ":
-					# The letter has been guessed, write it
-					print(guessedPhrase[index], end = "")
-		# Remove underlining because done
-		print("\u001b[m")
+				if guessedPhrase[index] == " ":
+					# Print an underscore to represent the hidden letter
+					# Have not won
+					haveWon = False
+					print("_", end = " ")
+				else:
+					print(guessedPhrase[index], end = " ")
+		if haveWon:
+			print("")
+			print("You won!")
+			break
+		print("")
+		print("")
+
 		# Ask for next letter
 		letter = str(input("Enter letter: "))
+		hasLetterBeenFound = False
+		count = 0
 		for index in range(len(secretPhrase)):
 			if secretPhrase[index] == letter:
-				# The letter has been guessed
-				tempText = list(guessedPhrase)
-				tempText[index] = letter
-				guessedPhrase = "".join(tempText)
+				count = count + 1
+				guessedPhrase[index] = letter
+				hasLetterBeenFound = True
+				if count == 1:
+					print("You have " + str(lives) + " lives remaining")
+					
+				
+		
+		# User didn't guess the right letter
+		if not hasLetterBeenFound:
+			lives = lives - 1
+			if lives == 0:
+				print("You literally have no life. You lost. What a loser")
+				break
+			print("Try a different letter")
+			print("You have " + str(lives) + " lives remaining")
+		
+		sleep(3) 
+		clear()
+
+	print("Finished")
+
 
 # Start the game
 startGame()
